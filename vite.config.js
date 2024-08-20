d570e6e45bb12cf79fb3e570bee36b5a619cd2dc
Moazzam-Sonu <99110720+Moazzam-Sonu@users.noreply.github.com>
2024-08-20 14:34:26 +0500

    Update vite.config.js
    update otdir

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // This changes the output directory from 'dist' to 'build'
+  },
 })
